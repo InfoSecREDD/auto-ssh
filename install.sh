e29d30810338f3d7fc54f3e67c7ec82bd2109526
ar1k88 <ar1k88gamingllc@gmail.com>
2018-12-04 22:06:42 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-mkdir /usr/local/auto-ssh
+git clone https://github.com/ar1k88/auto-ssh.git
+cd auto-ssh
+
+mkdir -p /usr/local/auto-ssh
 cp r r-exec r-push r-shell /usr/local/auto-ssh
-ln -sv /usr/local/auto-ssh/r /usr/local/bin/r
+ln -sfv /usr/local/auto-ssh/r /usr/local/bin/r
+rm -- "$0"
 echo -e "Done.."
 r -h
